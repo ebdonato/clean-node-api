0ee9cc9306af5e4c1337484716083a4a14f332f9
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-09 12:44:48 -0300

    chore: add jest and script configs

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,10 @@ module.exports = {
     // clearMocks: false,
 
     // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: true,
+    collectCoverage: false,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ["<root>/src/**/*.ts"],
+    collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
