6c4c875466eac38f39faa7e706c7ce35272ac3f1
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-09 10:35:49 -0300

    feat: and test: ensure SignUpController returns if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -102,6 +102,25 @@ describe("SignUp Controller", () => {
         expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
     })
 
+    test("Should return 400 if no password confirmation fails", () => {
+        const {sut} = makeSut()
+
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_name@mail.com",
+                password: "any_password",
+                passwordConfirmation: "invalid_confirmation_password",
+            },
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+
+        expect(httpResponse.body).toEqual(new InvalidParamError("passwordConfirmation"))
+    })
+
     test("Should return 400 if an invalid email is provided", () => {
         const {sut, emailValidatorStub} = makeSut()
 
