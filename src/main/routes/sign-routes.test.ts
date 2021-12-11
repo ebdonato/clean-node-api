b6bc7ca664527ad93aefec49fc323c50e0e3dd62
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-11 19:46:04 -0300

    feat: add SignUp route

---
+++ src/main/routes/sign-routes.test.ts
@@ -0,0 +1,29 @@
+import request from "supertest"
+import {MongoHelper} from "../../infra/db/mongodb/helpers/mongodb-helper"
+import app from "../config/app"
+
+describe("SignUp Routes", () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL as string)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        await MongoHelper.getCollection("accounts").deleteMany({})
+    })
+
+    test("Should return an account on success", async () => {
+        await request(app)
+            .post("/api/signup")
+            .send({
+                name: "any_name",
+                email: "any_name@mail.com",
+                password: "any_password",
+                passwordConfirmation: "any_password",
+            })
+            .expect(200)
+    })
+})
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,18 @@
-import app from "./config/app"
+import {MongoHelper} from "../infra/db/mongodb/helpers/mongodb-helper"
+import dotenv from "dotenv"
 
-app.listen(5050, () => console.log("Server is running at http://localhost:5050"))
+dotenv.config()
+
+const connectionString =
+    process.env.MONGO_URL ||
+    "mongodb://localhost:27017clean-node-api?readPreference=primary&directConnection=true&ssl=false"
+
+const port = process.env.PORT || 5050
+
+MongoHelper.connect(connectionString)
+    .then(async () => {
+        const app = (await import("./config/app")).default
+
+        app.listen(port, () => console.log("Server is running at http://localhost:5050"))
+    })
+    .catch(console.error)
