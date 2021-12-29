91cf09e0b1c88543de7bbb12be7ac3006f5ca493
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-29 00:32:01 -0300

    refactor: remove await from JwtAdapter because jwt.sign is syncronous

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -9,7 +9,8 @@ export class JwtAdapter implements Encrypter {
     }
 
     async encrypt(value: string): Promise<string> {
-        const accessToken = await jwt.sign({id: value}, this.secret)
+        // const accessToken = await jwt.sign({id: value}, this.secret)
+        const accessToken = jwt.sign({id: value}, this.secret)
 
         return accessToken
     }
