9d4b35fbb6b8880511d36c74941f86726a577d33
Eduardo Donato <eduardo.donato@gmail.com>
2022-01-15 15:34:20 -0300

    feat: ensure AccountMongoRepository finds an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -97,4 +97,35 @@ describe("Account Mongo Repository", () => {
             expect(account.accessToken).toBe("any_token")
         })
     })
+
+    describe("loadByToken()", () => {
+        test("Should return an account on loadByToken without role success", async () => {
+            const sut = makeSut()
+
+            await accountCollection.insertOne({
+                name: "any_name",
+                email: "any_name@mail.com",
+                password: "any_password",
+                accessToken: "any_token",
+            })
+
+            const account = await sut.loadByToken("any_token")
+
+            expect(account?.id).toBeTruthy()
+
+            expect(account.name).toBe("any_name")
+
+            expect(account.email).toBe("any_name@mail.com")
+
+            expect(account.password).toBe("any_password")
+        })
+
+        // test("Should return null with loadByEmail fails", async () => {
+        //     const sut = makeSut()
+
+        //     const account = await sut.loadByEmail("any_name@mail.com")
+
+        //     expect(account?.id).toBeFalsy()
+        // })
+    })
 })
--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,4 +1,5 @@
 import {AddAccountRepository} from "../../../../data/protocols/db/account/add-account-repository"
+import {LoadAccountAccountByTokenRepository} from "../../../../data/protocols/db/account/load-account-account-by-token-repository"
 import {LoadAccountByEmailRepository} from "../../../../data/protocols/db/account/load-account-by-email-repository"
 import {UpdateAccessTokenRepository} from "../../../../data/protocols/db/account/update-access-token-repository"
 import {AccountModel} from "../../../../domain/models/account"
@@ -6,7 +7,11 @@ import {AddAccountModel} from "../../../../domain/use-cases/add-account"
 import {MongoHelper} from "../helpers/mongodb-helper"
 
 export class AccountMongoRepository
-    implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository
+    implements
+        AddAccountRepository,
+        LoadAccountByEmailRepository,
+        UpdateAccessTokenRepository,
+        LoadAccountAccountByTokenRepository
 {
     async add(account: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection("accounts")
@@ -28,5 +33,13 @@ export class AccountMongoRepository
         const accountCollection = await MongoHelper.getCollection("accounts")
 
         await accountCollection.updateOne({_id: id}, {$set: {accessToken: token}})
+    }
+
+    async loadByToken(token: string, role?: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection("accounts")
+
+        const result = await accountCollection.findOne({accessToken: token, role})
+
+        return result && MongoHelper.map(result)
     }
 }
