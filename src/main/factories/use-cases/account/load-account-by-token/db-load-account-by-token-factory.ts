276efe8705adc75172e448a20192d052d0d9c88a
Eduardo Donato <eduardo.donato@gmail.com>
2022-01-15 16:25:52 -0300

    feat: ensuere add survey route returns 403 with no access token is provided

---
+++ src/main/factories/use-cases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,13 @@
+import {DbLoadAccountAccountByToken} from "../../../../../data/use-cases/load-account-by-token/db-load-account-by-token"
+import {LoadAccountByToken} from "../../../../../domain/use-cases/load-account-by-token"
+import {JwtAdapter} from "../../../../../infra/cryptography/jwt-adapter/jwt-adapter"
+import {AccountMongoRepository} from "../../../../../infra/db/mongodb/account/account-mongo-repository"
+import env from "../../../../config/env"
+
+export const makeDLoadAccountByToken = (): LoadAccountByToken => {
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+    const accountMongoRepository = new AccountMongoRepository()
+
+    return new DbLoadAccountAccountByToken(jwtAdapter, accountMongoRepository)
+}
