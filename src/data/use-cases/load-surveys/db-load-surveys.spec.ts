1c243deb32fa21f92e52491a94edd711f4f940a2
Eduardo Donato <eduardo.donato@gmail.com>
2022-01-16 02:20:27 -0300

    test: ensure DbLoadSurveys throws if LoadSurveysRepository throws

--- src/data/use-cases/load-surveys/db-load-surveys.spec.ts
+++ src/data/use-cases/load-surveys/db-load-surveys.spec.ts
@@ -78,4 +78,16 @@ describe("DbLoadSurveys", () => {
 
         expect(surveys).toEqual(makeFakeSurveys())
     })
+
+    test("Should throws if LoadSurveysRepository throws", async () => {
+        const {sut, loadSurveysRepositoryStub} = makeSut()
+
+        jest.spyOn(loadSurveysRepositoryStub, "loadAll").mockReturnValueOnce(
+            new Promise((resolve, reject) => reject(new Error()))
+        )
+
+        const promise = sut.load()
+
+        expect(promise).rejects.toThrow()
+    })
 })
