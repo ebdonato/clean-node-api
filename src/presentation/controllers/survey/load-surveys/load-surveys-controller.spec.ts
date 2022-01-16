0944dc400876c0f89c331306369a938035a8ffba
Eduardo Donato <eduardo.donato@gmail.com>
2022-01-16 01:34:37 -0300

    feat: ensure: LoadSurveyController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,5 +1,6 @@
-import {SurveyModel, LoadSurveys} from "./load-surveys-controller-protocols"
 import {LoadSurveysController} from "./load-surveys-controller"
+import {SurveyModel, LoadSurveys} from "./load-surveys-controller-protocols"
+import {okResponse} from "../../../helpers/http/http-helper"
 import MockDate from "mockdate"
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -68,5 +69,13 @@ describe("LoadSurveys Controller", () => {
         await sut.handle({})
 
         expect(loadSpy).toHaveBeenCalled()
+    })
+
+    test("Should return 200 on success", async () => {
+        const {sut} = makeSut()
+
+        const httpResponse = await sut.handle({})
+
+        expect(httpResponse).toEqual(okResponse(makeFakeSurveys()))
     })
 })
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,3 +1,4 @@
+import {okResponse} from "../../../helpers/http/http-helper"
 import {Controller, HttpRequest, HttpResponse, LoadSurveys} from "./load-surveys-controller-protocols"
 
 export class LoadSurveysController implements Controller {
@@ -6,8 +7,8 @@ export class LoadSurveysController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         httpRequest
 
-        await this.loadSurveys.load()
+        const surveys = await this.loadSurveys.load()
 
-        return null as unknown as HttpResponse
+        return okResponse(surveys)
     }
 }
