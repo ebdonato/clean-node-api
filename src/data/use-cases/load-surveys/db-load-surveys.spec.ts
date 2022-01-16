31540afde1e217c9d92c7b7b44d5ca6568064495
Eduardo Donato <eduardo.donato@gmail.com>
2022-01-16 02:16:26 -0300

    feat: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/use-cases/load-surveys/db-load-surveys.spec.ts
+++ src/data/use-cases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,7 @@
 import {SurveyModel} from "../../../domain/models/survey"
 import {LoadSurveysRepository} from "../../protocols/db/survey/load-surveys-repository"
 import {DbLoadSurveys} from "./db-load-surveys"
+import MockDate from "mockdate"
 
 const makeFakeSurveys = (): SurveyModel[] => {
     return [
@@ -52,6 +53,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe("DbLoadSurveys", () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+
     test("Should call LoadSurveysRepository", async () => {
         const {sut, loadSurveysRepositoryStub} = makeSut()
 
@@ -60,5 +69,13 @@ describe("DbLoadSurveys", () => {
         await sut.load()
 
         expect(loadAllSpy).toHaveBeenCalled()
+    })
+
+    test("Should call return a list of surveys on success", async () => {
+        const {sut} = makeSut()
+
+        const surveys = await sut.load()
+
+        expect(surveys).toEqual(makeFakeSurveys())
     })
 })
